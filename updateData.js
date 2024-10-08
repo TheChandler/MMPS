df047f5cb885271d8e8ffdb1e017f15bf32717b8
TheChandler <ChandlerMiller@gmail.com>
2024-10-07 20:26:59 -0400

    auto commit update data

--- updateData.js
+++ updateData.js
@@ -1,6 +1,7 @@
 const fs = require('node:fs')
 
-const { key } = require('./googleApiKey.json')
+const { key } = require('./googleApiKey.json');
+const { exec, execSync } = require('node:child_process');
 const sheetId = "17mOTV4LuIgdz_ui_pIzGC-vqDtIiFPXfDTh2l-r0EZA"
 
 // https://docs.google.com/spreadsheets/d/17mOTV4LuIgdz_ui_pIzGC-vqDtIiFPXfDTh2l-r0EZA/edit?gid=0#gid=0
@@ -8,6 +9,7 @@ const sheetId = "17mOTV4LuIgdz_ui_pIzGC-vqDtIiFPXfDTh2l-r0EZA"
 const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Sheet1?alt=json&key=${key}`
 
 
+const date = new Date().toISOString().split("T")[0]
 
 async function getData() {
 
@@ -19,12 +21,17 @@ async function getData() {
     }
 
     const body = await response.json();
-    const date = new Date().toISOString().split("T")[0]
+    
 
     fs.writeFileSync('./responseFiles/' + date + "_response.json", JSON.stringify(body));
     fs.writeFileSync("./responseFiles/latest_response.json", JSON.stringify(body))
 
 
 }
-
-getData()
+execSync('git pull')
+getData().then(()=>{
+    
+    execSync('git add .')
+    execSync(`git commit -m "${date} data update"`)
+    execSync('git push')
+})
